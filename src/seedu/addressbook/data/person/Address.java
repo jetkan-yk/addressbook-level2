a5adf10746de5989b108d9fc37aa2d8ed402a05b
jetkan-yk <jetkan.yk@gmail.com>
2019-02-14 02:23:04 +0800

    Update new address format
    New address format: BLOCK, STREET, UNIT, POSTAL CODE
    
    New ADDRESS_VALIDATION_REGEX that follows these rules:
     - Input must contain 3 commas corresponding to the 4 address info
     - BLOCK accepts optional "Block/Blk" followed by at most 3 numbers
       and 1 alphabet
     - STREET accepts any string input
     - UNIT accepts optional "#" followed by pattern like: #01 or 13a
       or 33-168C or 07-12D
     - POSTAL CODE must be a 6 numbers input

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -8,9 +8,9 @@ import seedu.addressbook.data.exception.IllegalValueException;
  */
 public class Address {
 
-    public static final String EXAMPLE = "123, some street";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+";
+    public static final String EXAMPLE = "Blk 123, Sin Ming Rd, #32-16A, 567120";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses should follow this format: BLOCK, STREET, UNIT, POSTAL CODE";
+    public static final String ADDRESS_VALIDATION_REGEX = "[a-zA-Z]{0,5}\\s*\\d{1,3}[a-zA-Z]?\\s*[,]\\s*[0-9a-zA-Z\\s]+\\s*[,]\\s*[#]?\\d{1,3}([-]\\d{1,3})?[a-zA-z]?\\s*[,]\\s*\\d{6}";
 
     public final String value;
     private boolean isPrivate;
